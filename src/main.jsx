1e189d8bf64111af10f99397ee11ea59d68c1828
Mohammed <mohammed.yabrag@hotmail.com>
2025-06-15 23:56:49 +0200

    removed HashRouter from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { HashRouter } from "react-router-dom";
+//import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>
 );
